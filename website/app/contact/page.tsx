d3b9710b2aef9d14759476c6124ceccae4d839b8
Samuel Sharivker <sam@ShmulPro.local>
2025-02-27 10:29:21 -0500

    form progress

--- website/app/contact/page.tsx
+++ website/app/contact/page.tsx
@@ -2,26 +2,43 @@
 
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <form>
+    <div className="text-center flex flex-col items-center m-5 gap-2">
+      <h1 className="text-5xl">Contact Page</h1>
+      <form className="flex flex-col items-center gap-3 border border-black p-10 bg-red-400">
         <div>
-          <p>First Name:</p>
-          <input type="text" placeholder="John" required />
+          <p className="text-2xl text-left">First Name:</p>
+          <input
+            className="text-xl p-2 rounded-lg border border-black"
+            type="text"
+            placeholder="John"
+            required
+          />
         </div>
         <div>
-          <p>Last Name:</p>
-          <input type="text" placeholder="Doe" required />
+          <p className="text-2xl text-left">Last Name:</p>
+          <input
+            className="text-xl p-2 rounded-lg border border-black"
+            type="text"
+            placeholder="Doe"
+            required
+          />
         </div>
         <div>
-          <p>Email:</p>
-          <input type="email" placeholder="jdoe@test.com" required />
+          <p className="text-2xl text-left">Email:</p>
+          <input
+            className="text-xl p-2 rounded-lg border border-black"
+            type="email"
+            placeholder="jdoe@test.com"
+            required
+          />
         </div>
         <div>
-          <p>Message:</p>
-          <textarea required />
+          <p className="text-2xl text-left">Message:</p>
+          <textarea className="w-60 border border-black resize-none" required />
         </div>
-        <button type="submit">Send Message</button>
+        <button className="bg-red-600 p-3 rounded-lg" type="submit">
+          Send Message
+        </button>
       </form>
     </div>
   );
