c70ef4f256b10c99fc058df308860ab3700fcc67
Samuel Sharivker <sam@ShmulPro.local>
2025-02-27 10:15:40 -0500

    created form

--- website/app/contact/page.tsx
+++ website/app/contact/page.tsx
@@ -4,6 +4,25 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
+      <form>
+        <div>
+          <p>First Name:</p>
+          <input type="text" placeholder="John" required />
+        </div>
+        <div>
+          <p>Last Name:</p>
+          <input type="text" placeholder="Doe" required />
+        </div>
+        <div>
+          <p>Email:</p>
+          <input type="email" placeholder="jdoe@test.com" required />
+        </div>
+        <div>
+          <p>Message:</p>
+          <textarea required />
+        </div>
+        <button type="submit">Send Message</button>
+      </form>
     </div>
   );
 }
