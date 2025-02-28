855b11e08f1ef402937a39d4c4cfd4415f1b8b1c
Samuel Sharivker <94409128+samSharivker@users.noreply.github.com>
2025-02-28 03:11:02 -0500

    deployment error fix

--- website/app/statistics/page.tsx
+++ website/app/statistics/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import fig1 from "@/public/img/figure1.png";
 
 export default function Statistics() {
-  content = "Based on the figure above, most survey participants believe schools should provide stronger diabetic education. Although 2.5% of participants strongly disagree with this sentiment, it is most likely due to a lack of knowledge or awareness of prediabetes or diabetes, which also explains the 16.5% of neutral participants who don't have a strong opinion for or against increasing diabetic education in NYC public schools.";
+  const content: string = "Based on the figure above, most survey participants believe schools should provide stronger diabetic education. Although 2.5% of participants strongly disagree with this sentiment, it is most likely due to a lack of knowledge or awareness of prediabetes or diabetes, which also explains the 16.5% of neutral participants who don't have a strong opinion for or against increasing diabetic education in NYC public schools.";
   return (
     <div className="text-center flex flex-col items-center m-5">
       <p>Statistics</p>
