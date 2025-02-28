a438fa080ddb9aea5e072f5d33a81866454506cc
Samuel Sharivker <sam@ShmulPro.local>
2025-02-28 02:51:16 -0500

    update

--- website/app/statistics/page.tsx
+++ website/app/statistics/page.tsx
@@ -1,9 +1,26 @@
 "use client";
 
+import Image from "next/image";
+import fig1 from "@/public/img/figure1.png";
+
 export default function Statistics() {
   return (
-    <div>
-      <h1>Statistics page</h1>
+    <div className="text-center flex flex-col items-center m-5">
+      <p>Statistics</p>
+      <div className="flex flex-col items-center">
+        <div>
+          <Image className="max-w-xl rounded-lg" src={fig1} alt="figure 1" />
+          <p>
+            Based on the figure above, most survey participants believe schools
+            should provide stronger diabetic education. Although 2.5% of
+            participants strongly disagree with this sentiment, it is most
+            likely due to a lack of knowledge or awareness of prediabetes or
+            diabetes, which also explains the 16.5% of neutral participants who
+            don't have a strong opinion for or against increasing diabetic
+            education in NYC public schools.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
